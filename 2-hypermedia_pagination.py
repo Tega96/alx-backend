3faa8023e4bcd15b0efc07c824621440b9aa6694
Tega96 <otiteotega@gmail.com>
2024-10-29 04:49:05 +0000

    Hypermedia pagination

--- 2-hypermedia_pagination.py
+++ 2-hypermedia_pagination.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env/ python3
+#!/usr/bin/env python3
 """ Helper function index_range that takes two int args."""
-from typing import Tuple, List
+from typing import Tuple, List, Dict, Any
 import csv
 import math
 
@@ -10,8 +10,8 @@ def index_range(page: int, page_size: int) -> Tuple:
     Calculate the start and end indices for pagination.
 
     Given a page number and the size of the page (number of items per page),
-    this function retruns a typle containing the start index and the end
-    index for that page.
+    this function returns a tuple containing the start index and the end index
+    for that page.
 
     Args:
     page (int): The page number (1-indexed).
@@ -20,19 +20,17 @@ def index_range(page: int, page_size: int) -> Tuple:
     Returns:
     tuple: A tuple containing the start index and the end index.
     """
-
     # Calculate the start index for the given page
     start_index = (page - 1) * page_size
 
-    # Calculate the start index for the given page
+    # Calculate the end index for the given page
     end_index = start_index + page_size
 
     return start_index, end_index
 
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
+    """Server class to paginate a database of popular baby names.
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
@@ -62,7 +60,11 @@ class Server:
         List[List]: The list of rows for the requested page.
         """
         assert isinstance(
-                page, int) and page > 0, "page must be an int, > 0"
+                page, int) and page > 0, \
+            "page must be an integer greater than 0"
+        assert isinstance(
+                page_size, int) and page_size > 0, \
+            "page_size must be an integer greater than 0"
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
@@ -71,3 +73,30 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """
+        Get a page from the dataset with additional pagination metadata.
+
+        Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+        Returns:
+        Dict[str, Any]: A dictionary containig the dataset page and metadata.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "page": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
---
+++ 2-main.py
@@ -0,0 +1,7 @@
+{'page_size': 2, 'page': 1, 'data': [['2016', 'FEMALE', 'ASIAN AND PACIFIC ISLANDER', 'Olivia', '172', '1'], ['2016', 'FEMALE', 'ASIAN AND PACIFIC ISLANDER', 'Chloe', '112', '2']], 'next_page': 2, 'prev_page': None, 'total_pages': 9709}
+---
+{'page_size': 2, 'page': 2, 'data': [['2016', 'FEMALE', 'ASIAN AND PACIFIC ISLANDER', 'Sophia', '104', '3'], ['2016', 'FEMALE', 'ASIAN AND PACIFIC ISLANDER', 'Emma', '99', '4']], 'next_page': 3, 'prev_page': 1, 'total_pages': 9709}
+---
+{'page_size': 3, 'page': 100, 'data': [['2016', 'FEMALE', 'BLACK NON HISPANIC', 'Londyn', '14', '39'], ['2016', 'FEMALE', 'BLACK NON HISPANIC', 'Amirah', '14', '39'], ['2016', 'FEMALE', 'BLACK NON HISPANIC', 'McKenzie', '14', '39']], 'next_page': 101, 'prev_page': 99, 'total_pages': 6473}
+---
+{'page_size': 0, 'page': 3000, 'data': [], 'next_page': None, 'prev_page': 2999, 'total_pages': 195}
